fe55aad878841221b236aa844d6b6f34a4969751
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2019-02-14 14:05:20 +0300

    Добавлена ф-я GetMoeDeloData
     GetMoeDeloData($url, $params)

--- MoeDeloFunctions.php
+++ MoeDeloFunctions.php
@@ -8,6 +8,26 @@ require_once 'common.php';
 
     // функции
 
-
+    // функция возвращает данные по запросу (string)$url с параметрами (array)$params
+function GetMoeDeloData($url, $params) {
+    // проверка наличия входных данных
+  if (!$url) return false;
+  $url = MOEDELO_URL.$url;
+  if ($params) $url .= '?'.http_build_query($params);
+  $curl = curl_init($url);
+  curl_setopt_array($curl, array(
+    CURLOPT_HTTPHEADER => array('Accept: application/json', 'md-api-key: '.MOEDELO_TOKEN, 'Content-Type: application/json'),
+    CURLOPT_RETURNTRANSFER => true
+  ));
+  if ($response=curl_exec($curl)) if ($answer=json_decode($response,true)) { curl_close($curl); return $answer; }             
+  curl_close($curl);
+  return false;
+}
+	
+	
+	
+	
+	
+	
 
 ?>
