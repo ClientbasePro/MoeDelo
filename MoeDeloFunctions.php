2262ab28e17d64b5a46caa9f7c7c68a05f433915
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2019-02-14 13:47:56 +0300

    Добавлен репозиторий MoeDelo

---
+++ MoeDeloFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+  // Интеграция CRM Clientbase и Моё Дело
+  // https://ClientbasePro.ru
+  // https://restapi.moedelo.org/s/?url=/docs
+  
+require_once 'common.php';
+
+    // функции
+
+
+
+?>
